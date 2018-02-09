0bdbff038b62f4b303e114acf9db36646c554844
sudhakarjs <30828414+sudhakarjs@users.noreply.github.com>
2018-02-09 14:44:18 +0530

    Create 62.c

---
+++ 62.c
@@ -0,0 +1,13 @@
+int main(void) {
+           int a[100],c,max=0;
+         for(c=0;c<10;c++)
+           scanf("%d",&a[i]);
+           max=a[0];
+           for(c=0;c<10;c++)
+           {
+           	if(a[c]>max)
+           	max=a[c];
+           }
+           printf("%d",max);
+	return 0;
+}
