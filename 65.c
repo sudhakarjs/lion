0a1212a11ac466f38a0a998a47a03a5fbac0eef4
sudhakarjs <30828414+sudhakarjs@users.noreply.github.com>
2018-02-13 09:03:22 +0000

    Create 65.c

---
+++ 65.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(void)
+{
+	int i,j,add;
+	scanf("%d",&i);
+	scanf("%d",&j);
+	add=i+j;
+	if(add%2==0)
+	{
+		printf("even");
+	}
+	else
+	printf("odd");
+    
+   return 0;
+}
