603dcc7073d4d98f5a2bb57b90901eea12310592
sudhakarjs <30828414+sudhakarjs@users.noreply.github.com>
2018-02-13 08:48:37 +0000

    Create 63.c

---
+++ 63.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void main()
+{
+char n[20],i,count=0;
+printf("enter the number");
+scanf("%s",&n);
+for(i=0;n[i]!='\0';i++)
+{
+if(!(n[i]==(0||1)))
+{
+count++;
+}
+}
+if(count==0)
+{
+printf("the number is binary");
+}
+else
+{
+printf("the number is not a binary");
+}
+}
+
